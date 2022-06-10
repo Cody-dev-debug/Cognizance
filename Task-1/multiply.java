011918a452093734e448a39c3c0931a1ae084e18
Aditya <bansaladitya150@gmail.com>
2022-06-11 04:23:06 +0530

    Branch commit 1

---
+++ Task-1/multiply.java
@@ -0,0 +1,11 @@
+public class multiply
+{
+     public static void main(String[] args)
+     {
+          int a,b,mut;
+          a=18;
+          b=45;
+          mut=a*b;
+          System.out.println("Multiplication = "+mut);
+     }
+}
