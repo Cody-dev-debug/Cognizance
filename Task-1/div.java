71d4aa0a30b1cc7fcde1ede7399b943d2ab1a333
Aditya <bansaladitya150@gmail.com>
2022-06-11 04:29:00 +0530

    Branch commit 2

---
+++ Task-1/div.java
@@ -0,0 +1,12 @@
+public class div
+{
+     public static void main(String[] args)
+     {
+          int a,b;
+          double div;
+          a=156;
+          b=8;
+          div=a/b;
+          System.out.println("Division = "+div);
+     }
+}
